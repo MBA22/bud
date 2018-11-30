1f97974099331a66a06b24c80f6749dd0030b8a4
Mario Barragán <mario.barragan.2012@gmail.com>
2018-11-30 02:07:36 -0600

    List model

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,20 @@
+class ListsController < ApplicationController
+	def show
+		@list = List.find(params[:id])
+	end
+
+	def new
+	end
+
+	def create
+		@list = List.new(list_params)
+
+		@list.save
+		redirect_to @list
+	end
+
+	private
+	  def list_params
+	  	params.require(:list).permit(:name, :string)
+	  end
+end
---
+++ db/migrate/20181130075753_create_lists.rb
@@ -0,0 +1,10 @@
+class CreateLists < ActiveRecord::Migration[5.2]
+  def change
+    create_table :lists do |t|
+      t.string :name
+      t.text :email
+
+      t.timestamps
+    end
+  end
+end
